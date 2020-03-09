351f8cd283847501b550edd372f95c9d0c362560
Sonal Agawane <sonal@scriptlanes.com>
2020-03-09 14:39:17 +0530

    Initial Commit

---
+++ koml_machinetest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  koml_machinetest
+//
+//  Created by Script Lanes on 09/03/20.
+//  Copyright © 2020 Script Lanes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
